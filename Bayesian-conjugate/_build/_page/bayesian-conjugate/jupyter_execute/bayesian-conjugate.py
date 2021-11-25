4785cd45e45feedec346d5b7067258287ab2f1a2
Tianxu Jia <jiatianxu@hotmail.com>
2021-11-25 15:08:03 +0000

    New blog

---
+++ Bayesian-conjugate/_build/_page/bayesian-conjugate/jupyter_execute/bayesian-conjugate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 
+
+# <article>
+#     <header>
+#         <h1 class="headline">Conjugate Function makes Bayesian Awsome</h1>
+#         <div class="byline vcard">
+#             <address class="author">By <a rel="author" class="url fn n" href="https://www.linkedin.com/in/tianxu-jia-a2787917/">Tianxu Jia</a>   
+#                 <p> Station10 Ltd</p>   
+# </article>
+#             <p> 2021/12/28</p>
+
+# # 1. Stroy
+
+# # 2. From the Story to Bayesian Formular
+
+# # 3. Is here someting wrong with Bayesian?
+
+# # 4 The conjugate Function
+
+# # 5. Awsome, Bayesian!
+
+# In[ ]:
+
+
+
+
